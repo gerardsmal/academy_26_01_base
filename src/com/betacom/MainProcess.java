25322f88a9d82906f5e4cf2c30e614bb22f596bf
Gérard Smal <g.smal@betacom.it>
2026-01-29 13:07:43 +0100

    refelction

--- src/com/betacom/MainProcess.java
+++ src/com/betacom/MainProcess.java
@@ -10,6 +10,7 @@ import com.betacom.process.ProcessDate;
 import com.betacom.process.ProcessEnum;
 import com.betacom.process.ProcessException;
 import com.betacom.process.ProcessInt;
+import com.betacom.process.ProcessReflection;
 import com.betacom.process.ProcessSequential;
 import com.betacom.process.ProcessSingleTone;
 import com.betacom.process.ProcessStream;
@@ -22,9 +23,9 @@ public class MainProcess {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-//		System.out.print("Introdure un parametro [interface,string,exception, date, enum, collection, singletone, sequential, anonima, stream]:");
+//		System.out.print("Introdure un parametro [interface,string,exception, date, enum, collection, singletone, sequential, anonima, stream, reflection]:");
 //		String inp = sc.nextLine();
-		String inp = "stream";
+		String inp = "reflection";
 		Map<String,ProcessInterface> pr = new HashMap<String, ProcessInterface>();
 		pr.put("string", new StringProcess());
 		pr.put("exception", new ProcessException());
@@ -36,6 +37,7 @@ public class MainProcess {
 		pr.put("sequential", new ProcessSequential());
 		pr.put("anonima", new ProcessAnonima());
 		pr.put("stream", new ProcessStream());
+		pr.put("reflection", new ProcessReflection());
 			
 		if (pr.containsKey(inp)) {
 			try {
---
+++ src/com/betacom/process/ProcessReflection.java
@@ -0,0 +1,103 @@
+package com.betacom.process;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+
+import com.betacom.interfaces.ProcessInterface;
+
+public class ProcessReflection implements ProcessInterface{
+
+	@Override
+	public boolean execute() throws Exception {
+		System.out.println("Begin ProcessReflection");
+		
+		String packageName = "com.betacom.objects";
+		String className   = "MyClassReflection";
+		
+		try {
+			Class cl = Class.forName(packageName + "." + className); 
+			System.out.println("Class " + className + " found");
+			
+			/*
+			 * retrieve contructors for selected class
+			 */
+			Constructor[] ctore = cl.getConstructors();
+			Constructor  selectedConstructor = null;
+			int constructorToSelect = 0;
+			Object myClass = null;
+			
+			System.out.println("numero di contructors trovati:" + ctore.length);
+			
+			for (Constructor ctore1:ctore) {
+				/*
+				 * search parameters number for all constructors
+				 */
+				System.out.println("..Parameters number for constuctor:" + ctore1.getParameterCount());
+				
+				if (ctore1.getParameterCount() == constructorToSelect) {
+					selectedConstructor = ctore1;
+				}
+				
+				Type[] type = ctore1.getGenericParameterTypes();
+				for (Type t:type) {
+					System.out.println("....Parameter type:" + t);
+				}
+			}
+
+			/*
+			 * new instance
+			 */
+			if (constructorToSelect == 0) {
+				myClass = selectedConstructor.newInstance();
+				System.out.println("select constructor without parameter");
+			}
+			
+			if (constructorToSelect == 2) {
+				myClass = selectedConstructor.newInstance(10, "Description numero 1");
+				System.out.println("select constructor with 2 parameters");
+			}
+		
+			if (constructorToSelect == 3) {
+				myClass = selectedConstructor.newInstance(10, "Description numero 2", 20);
+				System.out.println("select constructor with 3 parameters");
+			}
+			
+			
+			/*
+			 * introspection methods
+			 */
+			Method[] methods = myClass.getClass().getMethods();
+			for (Method method:methods) {
+				System.out.println("... method found:" + method.getName());
+				
+				if ("setId".equals(method.getName())) {
+					method.invoke(myClass, 99);
+				}
+
+				if ("setDesc".equals(method.getName())) {
+					method.invoke(myClass, "description caricato dentro loop");
+				}
+
+				if ("setP1".equals(method.getName())) {
+					method.invoke(myClass, 99999);
+				}
+
+			}
+			
+			String methodName = "toString";
+			Method metodo = myClass.getClass().getMethod(methodName);
+			String msg = (String)metodo.invoke(myClass);
+			
+			System.out.println(msg);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException:" + e.getMessage() );
+		} catch (IllegalArgumentException e) {
+			System.out.println("IllegalArgumentException:" + e.getMessage() );
+		}
+		
+		return false;
+	}
+
+}
